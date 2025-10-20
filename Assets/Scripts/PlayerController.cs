5cfa7c6aab28800f00c17e96e02dd34875f69c9e
Dong2003 <147420281+Dong032k3@users.noreply.github.com>
2025-10-20 17:54:07 +0700

    Update boost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController Instance;
+
     private Rigidbody2D rb;
+    private Animator animator;
     private Vector2 playerDirection;
     [SerializeField] private float moveSpeed = 5f;
+    public float boost = 1f;
+    private float boostPower = 5f;
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         
     }
 
@@ -15,11 +33,33 @@ public class PlayerController : MonoBehaviour
     {
         float directionX = Input.GetAxisRaw("Horizontal");
         float directionY = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("moveX", directionX);
+        animator.SetFloat("moveY", directionY);
+
         playerDirection = new Vector2(directionX, directionY).normalized;
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire2"))
+        {
+            EnterBoost();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) || Input.GetButtonUp("Fire2"))
+        {
+            ExitBoost();
+        }
     }
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(playerDirection.x*moveSpeed, playerDirection.y*moveSpeed);
-        
+        rb.linearVelocity = new Vector2(playerDirection.x * moveSpeed, playerDirection.y * moveSpeed);
+
+    }
+    private void EnterBoost()
+    {
+        animator.SetBool("boosting", true);
+        boost = boostPower;
+    }
+    private void ExitBoost()
+    {
+        animator.SetBool("boosting", false);
+        boost = 1f;
     }
 }
